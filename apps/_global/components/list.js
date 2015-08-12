621d0d2acaa5c64f2d314d0f1f380093a2622757
Kristian Mandrup <kmandrup@gmail.com>
2015-08-12 11:57:36 +0100

    better custom tags

---
+++ apps/_global/components/list.js
@@ -0,0 +1,5 @@
+// icon-list-item,main-menu,page-title,repo-activity,search-box,session-menu,team-activity,user-activity
+module.exports = [
+  'icon-list-item',
+  ''
+]
--- mount/self.js
+++ mount/self.js
@@ -2,7 +2,6 @@
 let marooka = require('marooka');
 let path = require('path');
 let rootPath = path.resolve(path.join(__dirname, '../'));
-
 // mounting multiple apps on appContainer instance
 let apps = ['index'];
 module.exports = new marooka.AppMounter(rootPath).mountApps(apps).start();
